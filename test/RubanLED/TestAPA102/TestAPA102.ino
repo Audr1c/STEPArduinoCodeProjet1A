14ca77733f2f74089596b77e1819e5261fb7c48b
Audr1c <audric.siguret@gmail.com>
2025-04-05 20:04:00 +0200

    suite Apa102

--- test/RubanLED/TestAPA102/TestAPA102.ino
+++ test/RubanLED/TestAPA102/TestAPA102.ino
@@ -8,6 +8,7 @@ struct pix
 
 const int datapin = 22;
 const int clockpin = 23;
+pix pixels[300];
 
 void setup()
 {
@@ -21,7 +22,14 @@ void loop()
 
 void writeOct(byte octet)
 {
-
+    for (char i = 0; i < 8; i++)
+    {
+        digitalWrite(clockpin, LOW);
+        digitalWrite(datapin, octet & 1);
+        octet = octet >> 1;
+        digitalWrite(clockpin, HIGH);
+        delayMicroseconds(2);
+    }
 }
 
 void writeFrame(struct pix)
@@ -29,7 +37,7 @@ void writeFrame(struct pix)
     writeOct(pix.r);
     writeOct(pix.g);
     writeOct(pix.b);
-    writeOct(pix.alpha|0b11100000);
+    writeOct(pix.alpha | 0b11100000);
 }
 
 void writeArray(struct pix[300], uint16_t length)
