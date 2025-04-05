2f56043441c486e516562b5d2cac7396792ec7bc
valentin coullon <coullon.valentin@gmail.com>
2025-04-05 18:59:26 +0200

    finishedtestdistancesensor

--- test/TestDistSensor/TestSensor/TestSensor.ino
+++ test/TestDistSensor/TestSensor/TestSensor.ino
@@ -1,21 +1,22 @@
+const int triggerPin = 10, echoPin = 11;
+unsigned long duration = 0, distance = 0;
+
 void setup()
 {
+    pinMode(triggerPin, OUTPUT);
+    digitalWrite(triggerPin, LOW);
 
-    pinMode(trigPin, OUTPUT); // Définit le trigPin comme sortie
-    pinMode(echoPin, INPUT); // Définit le echoPin comme entrée
-    Serial.begin(9600); // Commence la communication série
+    pinMode(echoPin, INPUT);
+    Serial.begin(9600);
 }
 
 void loop()
-{ // Efface le trigPin
-    digitalWrite(trigPin, LOW);
-    delayMicroseconds(2); // Met le trigPin à l'état HIGH pendant 10 microsecondes
-    digitalWrite(trigPin, HIGH);
-    delayMicroseconds(10);
-    digitalWrite(trigPin, LOW);        // Lit le echoPin, renvoie le temps de trajet de l'onde sonore en microsecondes
-    duration = pulseIn(echoPin, HIGH); // Calcul de la distance
-    distance = duration * 0.034 / 2;   // La vitesse du son est d'environ 0.034 cm par microseconde
-    // Affiche la distance sur le moniteur série
+{
+    digitalWrite(triggerPin, HIGH);
+    delayMicroseconds(10); 
+    digitalWrite(triggerPin, LOW);        
+    duration = pulseIn(echoPin, HIGH, 10000);
+    distance = duration * 0.17;
     Serial.print("Distance: ");
     Serial.println(distance);
 }
